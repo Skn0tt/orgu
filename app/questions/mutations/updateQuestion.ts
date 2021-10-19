97ea9e8d7d3d39bb08ac9bd34fe1b4038c3cb24d
Tom Richter <tomrichter99@gmail.com>
2021-10-19 11:57:54 +0200

    fix updateQuestion

--- app/questions/mutations/updateQuestion.ts
+++ app/questions/mutations/updateQuestion.ts
@@ -3,6 +3,18 @@ import db from "db"
 import { UpdateQuestionSchema } from "../types"
 
 export default resolver.pipe(resolver.zod(UpdateQuestionSchema), async (question) => {
-  const updatedQuestion = await db.question.update({ where: { id: question.id }, data: question })
+  const updatedQuestion = await db.question.update({
+    where: { id: question.id },
+    data: { title: question.title, status: question.status },
+  })
+  await db.assignment.deleteMany({ where: { questionId: question.id } })
+  for (const personId of Array.from(question.assignedToPersonIds)) {
+    await db.assignment.create({
+      data: {
+        questionId: updatedQuestion.id,
+        personId,
+      },
+    })
+  }
   return updatedQuestion
 })
