471080793b0ed694dec36fc83f9dd10c94be2506
rsommerfeld <romeo.sommerfeld@web.de>
2021-10-24 15:52:35 +0200

    "fix" weird prisma TScompile error

--- app/auth/utils/authorize.ts
+++ app/auth/utils/authorize.ts
@@ -3,7 +3,7 @@ import { Ctx, SessionContext } from "blitz"
 // custom resolver.authorize
 const authorize = () => {
   return function _innerAuthorize(input: unknown, ctx: Ctx) {
-    const session: SessionContext = ctx.session
+    const session: SessionContext = (ctx as any).session
     if (process.env.BLITZ_PUBLIC_AUTHENTICATION_REQUIRED === "true") {
       session.$authorize("ADMIN")
     }
